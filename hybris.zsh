5e1a387ae30ad10e3e31bc9b2932afdaa4a9b7b5
maudin <maudin@bearstech.com>
2019-03-22 10:28:28 +0100

    see previous commit

--- hybris.zsh
+++ hybris.zsh
@@ -4,21 +4,16 @@ function precmd() {echo -n -e "\a"}
 # do not share history between terms...
 unsetopt share_history
 
-# Load keychain
-# keychain -q --agents ssh id_rsa id_dsa
-
 # Everyday aliases
-c-cat()          {for a in $@; do colorize_via_pygmentize $a; done}
-c-less()         {for a in $@; do colorize_via_pygmentize $a | less; done}
+cless()          {for a in $@; do ccat $a | less; done}
 e()              {emacsclient -n $@ > /dev/null 2>&1}
-health()	 {/usr/bin/screen -O -S health -c ~/.config/screen_health}
 j()		 {if [ $# -eq 0 ]; then marks; else jump $@; fi}
 loop()		 {while [ 1 ]; do sh -c "$@"; done}
 open_ports()	 {sudo netstat -tulpen 2> /dev/null | grep "LISTEN"}
-r()		 {if [ $# -eq 1 ]; then ssh root@$1; else sudo su -; fi}
+r()		 {if [ $# -gt 0 ]; then for h in $@; do ssh root@$h; done; else sudo su -; fi}
 dontknow()       {echo -n "¯\\_(ツ)_/¯" | xclip}
-def()            {echo "default_path = '$PWD'" | awesome-client}
 dchroot()        {docker run --rm --privileged -ti $@ debian:stretch /bin/bash}
+sc-error()       {xdg-open "https://github.com/koalaman/shellcheck/wiki/$1"}
 
 # Usefull bindings
 bindkey "^[Od" backward-word
